b4a4c79e72314afe53fc20756d93b0720fa7f96d
François <francoismonniot@gmail.com>
2020-07-10 17:23:02 -0700

    switch to iced for the gui part

---
+++ src/data.rs
@@ -0,0 +1,68 @@
+// Data model for the save game
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Ref {
+    #[serde(alias = "$ref")]
+    reference: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Party {
+    #[serde(alias = "$id")]
+    id: String,
+    #[serde(alias = "SceneName")]
+    scene_name: String,
+    #[serde(alias = "m_EntityData")]
+    entities: Vec<Entity>,
+    #[serde(alias = "HasEntityData")]
+    has_entity_data: bool
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Entity {
+    #[serde(alias = "Descriptor")]
+    descriptor: Descriptor
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Descriptor {
+    #[serde(alias = "Stats")]
+    stats: HashMap<String, StatOrRef>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Stat {
+    #[serde(alias = "$id")]
+    id: String,
+    #[serde(alias = "Type")]
+    tpe: String,
+    #[serde(alias = "m_BaseValue")]
+    base_value: i16
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum StatOrRef {
+    Ref(Ref),
+    Stat(Stat),
+    #[serde(alias = "$id")]
+    Id(String)
+}
+
+pub fn read_entity_from_path<P: AsRef<Path>>(path: P) -> Result<Entity, Box<Error>> {
+    // Open the file in read-only mode with buffer.
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let u = serde_json::from_reader(reader)?;
+
+    // Return the `User`.
+    Ok(u)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,99 @@
-extern crate azul;
+use iced::{button, Align, Button, Column, Element, Sandbox, Settings, Text, Row, Length, Container};
+mod data;
 
-use azul::{prelude::*, widgets::{label::Label, button::Button}};
-
-struct DataModel {
-  counter: usize,
+pub fn main() {
+    let kaylin = data::read_entity_from_path("samples/kaylin.json").unwrap();
+    //println!("Reading Kaylin data:\n{:#?}", kaylin);
+    Counter::run(Settings::default())
 }
 
-impl Layout for DataModel {
-    fn layout(&self, _info: LayoutInfo) -> Dom<Self> {
-        let label = Label::new(format!("{}", self.counter)).dom();
-        let button = Button::with_label("Update counter").dom()
-            .with_callback(On::MouseUp, |cb_info: CallbackInfo<Self>| {
-                cb_info.state.counter += 1;
-                Redraw
-            });
+#[derive(Default)]
+struct Counter {
+    value: i32,
+    increment_button: button::State,
+    decrement_button: button::State,
+}
 
-        let dom = Dom::div()
-            .with_child(label)
-            .with_child(button);
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
 
-        println!("dom:\r\n{}", dom.debug_dump());
-        dom
+type MyRow<'a> = Row<'a, Message>;
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
     }
-}
 
-fn main() {
-    let app = App::new(DataModel { counter: 0 }, AppConfig::default()).unwrap();
-    app.run(WindowCreateOptions::new(css::native()));
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+
+        let menu = Column::new()
+            .align_items(Align::Start)
+            .width(Length::from(50))
+            .push(Text::new("Party"));
+
+        let characters = Column::new()
+            .width(Length::from(150))
+            .push(Text::new("Kaylin"))
+            .push(Text::new("Solace"))
+            .push(Text::new("Amiri"))
+            .push(Text::new("Ember"));
+
+        // Statistics
+
+        let main_stats = Row::new()
+            .width(Length::Fill)
+            .height(Length::from(50))
+            .push(Text::new("Money: 38747G"))
+            .push(Text::new("Experience: 38747"))
+            .push(Text::new("Alignment: Neutral Good"));
+
+        let abilities_stats = Column::new()
+            .height(Length::Fill)
+            .push(Text::new("STR 10"))
+            .push(Text::new("DEX 15"));
+
+        let combat_stats = Column::new()
+            .push(Text::new("CMB 5"))
+            .push(Text::new("CMD 7"));
+
+        let skills_stats = Column::new()
+            .push(Text::new("Athletics 7"))
+            .push(Text::new("Mobility 13"));
+
+        let statistics = Row::new()
+            .push(abilities_stats)
+            .push(combat_stats)
+            .push(skills_stats);
+
+        let character = Column::new()
+            .width(Length::Fill)
+            .push(main_stats)
+            .push(statistics);
+
+        Row::new()
+            .push(menu)
+            .push(characters)
+            .push(character)
+            .into()
+    }
 }
