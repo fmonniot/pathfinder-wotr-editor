e28bb22a9f3f6ad5beb139355b0bd16f90892545
François Monniot <francoismonniot@gmail.com>
2020-07-14 19:35:50 -0700

    Add first data structures on json updates
    Let's go with a series of JsonPatch that we can accumulate
    on the application side, and then apply when we want to effectively
    edit the save game.

--- src/data.rs
+++ src/data.rs
@@ -281,11 +281,95 @@ impl IndexedJson {
         }
     }
 
-    /* When we start doing actual modification
-    pub fn pointer_mut(&mut self, pointer: &str) -> Option<&mut Value> {
-        self.json.pointer_mut(pointer)
+    // While nice to write, this isn't actually useful to us :)
+    /*
+    fn dereference_mut<'a>(&'a mut self, value: &'a mut Value, path: &str) -> Result<&'a mut Value, JsonReaderError> {
+        let sta = match value.as_object_mut() {
+            Some(v) => Ok(v),
+            None => Err(JsonReaderError::ObjectExpected(path.to_string(), reader::json_type(value).to_string()))
+        }?;
+
+        match sta.get_mut("$ref").and_then(|j| j.as_str()) {
+            Some(reference) => {
+                let pointer = self.index.get(reference).ok_or_else(|| {
+                    JsonReaderError::InvalidReference(path.to_string(), reference.to_string())
+                })?;
+
+                self.json.pointer_mut(pointer).ok_or_else(|| {
+                    JsonReaderError::InvalidReference(path.to_string(), reference.to_string())
+                })
+            },
+            None => Ok(value),
+        }
     }
     */
+
+    //* When we start doing actual modification
+    fn pointer_mut(&mut self, pointer: &str) -> Option<&mut Value> {
+        self.json.pointer_mut(pointer)
+    }
+    //*/
+
+    // TODO Might need something else that Reader, or rename Reader to a more generic term
+    pub fn patch(&mut self, patch: JsonPatch) -> Result<(), JsonReaderError> {
+
+        match patch {
+            JsonPatch::Id { id, new_value } => {
+                // Clone the pointer to release the immutable reference to self
+                let pointer = self.index.get(&id).unwrap().clone(); // TODO Error management
+
+                let value = self.pointer_mut(&pointer).unwrap();
+
+                // TODO Check the $id field is present in the new value
+                *value = Value::Object(new_value);
+
+                Ok(())
+            }
+            JsonPatch::Pointer { pointer, new_value } => {
+                let value = self.pointer_mut(&pointer).unwrap();
+
+                *value = new_value;
+                
+                Ok(())
+            }
+        }
+    }
+}
+
+pub enum JsonPatch {
+    Id {
+        id: String,
+        new_value: serde_json::Map<String, Value>
+    },
+    Pointer {
+        pointer: String,
+        new_value: Value
+    }
+}
+
+impl JsonPatch {
+    // Can only work with pointer, not with $id
+    // When working with $id, we need an object (which would include the $id field, although we can it ourselves)
+    pub fn u64(pointer: String, value: u64) -> JsonPatch {
+        JsonPatch::Pointer {
+            pointer, new_value: serde_json::to_value(value).unwrap()
+        }
+    }
+
+    pub fn by_id(id: String, json: Value) -> Result<JsonPatch, JsonReaderError> {
+        let mut json = match json {
+            Value::Object(map) => Ok(map),
+            _ => Err(JsonReaderError::ObjectExpected(id.clone(), reader::json_type(&json).to_string()))
+        }?;
+
+        if json.get("$id").is_none() {
+            json.insert("$id".to_string(), Value::String(id.clone()));
+        }
+
+        Ok(JsonPatch::Id {
+            id, new_value: json
+        })
+    }
 }
 
 fn build_index(json: &Value, path: &str, index: &mut BTreeMap<String, String>) {
@@ -373,5 +457,65 @@ mod reader {
             Value::Array(..) => "array",
             Value::Object(..) => "object",
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn fixtures() -> (Value, Value) {
+        let base: Value = serde_json::from_str(r#"
+        {
+            "$id": "1",
+            "other": {
+                "$id": "2",
+                "value": 42
+            }
+        }"#).unwrap();
+        let expected: Value = serde_json::from_str(r#"
+        {
+            "$id": "1",
+            "other": {
+                "$id": "2",
+                "value": 7
+            }
+        }"#).unwrap();
+
+        (base, expected)
+    }
+
+
+    #[test]
+    fn indexed_json_can_patch_u64_by_pointer() {
+        let (base, expected) = fixtures();
+        let mut index = IndexedJson::new(base);
+        let patch = JsonPatch::u64("/other/value".to_string(), 7);        
+
+        index.patch(patch).unwrap();
+
+        assert_eq!(index.json, expected);
+    }
+
+    #[test]
+    fn indexed_json_can_patch_by_id_with_id_in_patch() {
+        let (base, expected) = fixtures();
+        let mut index = IndexedJson::new(base);
+        let patch = JsonPatch::by_id("2".to_string(), serde_json::from_str(r#"{"$id":"2","value":7}"#).unwrap()).unwrap();        
+
+        index.patch(patch).unwrap();
+
+        assert_eq!(index.json, expected);
+    }
+
+    #[test]
+    fn indexed_json_can_patch_by_id_without_id_in_patch() {
+        let (base, expected) = fixtures();
+        let mut index = IndexedJson::new(base);
+        let patch = JsonPatch::by_id("2".to_string(), serde_json::from_str(r#"{"value":7}"#).unwrap()).unwrap();        
+
+        index.patch(patch).unwrap();
+
+        assert_eq!(index.json, expected);
     }
 }
